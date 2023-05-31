7acbc4397e7eb7245a42ee6ca49ab126aff73873
minngaeng <minngaeng@gmail.com>
2023-05-31 22:23:35 +0900

    0531

--- class/05.function.js
+++ class/05.function.js
@@ -21,3 +21,20 @@ const auth = () => {
     }
   }, 1000);
 };
+
+//인증번호 전송
+//난수 생성
+let token = String(Math.floor(Math.random() * 1000000)).padStart(6, '0');
+
+
+//타이머 만들어야함
+
+let time = 180
+setInterval(
+  function(){
+if (time >= 0){
+  let min = time /60
+}
+
+  }
+  ,1000)
