270438cf6f626600dc043ec432c4640f90cc3558
minngaeng <minngaeng@gmail.com>
2023-06-02 12:59:23 +0900

    0602

--- class/final/final.js
+++ class/final/final.js
@@ -1,9 +1,6 @@
-let phone1 = document.getElementById('phone1').value;ㅋㅋ
-let phone2 = document.getElementById('phone2').value;
-let phone3 = document.getElementById('phone3').value;
-
-const ph1 = () => {
+let phone1 = document.getElementById('phone1').value;
+function ph1() {
   if (phone1.length === 3) {
     document.getElementById('phone2').focus();
   }
-};
+}
